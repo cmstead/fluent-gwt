12dfca4d7efb57522679ffd92f62e1a7984285c1
Chris Stead <cmstead@gmail.com>
2020-02-25 14:10:57 -0800

    Added types to d.ts file

--- fluent-gwt.d.ts
+++ fluent-gwt.d.ts
@@ -1,7 +1,11 @@
 export as namespace fluentGwt;
 
 export function given(description: string, handler: () => any): whenActable
-export function arrange (description: string, handler: () => any): whenActable
+export function arrange(description: string, handler: () => any): whenActable
+export function fromCallback(handler: callbackResolvingHandler): (any) => Promise<any>
+
+declare type callbackResolvingHandler = (...args, callback: nodeCallback) => void;
+declare type nodeCallback = (error: any, data: any) => void;
 
 declare interface whenActable {
     when: (description: string, handler: (any) => any) => thenAssertable
